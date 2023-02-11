b9a8114c50773d8c7a5af00320a44884a20fbc62
puneethcm <puneethkumarcm8050@gmail.com>
2023-02-11 18:26:46 +0530

    Simple linked list

---
+++ LinkedList/CustomLinkedList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace LinkedList
+{
+	public class CustomLinkedList
+	{
+        public Node head;
+        internal void Add(int data)
+        {
+            Node newNode = new Node(data);
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = newNode;
+            }
+            Console.WriteLine("{0} is Inserted into Linked List", newNode.data);
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("\nLinked List is Empty ");
+            }
+            Console.WriteLine("\nDisplaying Nodes: ");
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
+
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -1,8 +1,15 @@
-﻿internal class Program
+﻿using LinkedList;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Welcome To Linked List");
+        CustomLinkedList linkedlist = new CustomLinkedList();
+        linkedlist.Add(56);
+        linkedlist.Add(30);
+        linkedlist.Add(70);
+        linkedlist.Display();
         Console.ReadLine();
     }
 }
